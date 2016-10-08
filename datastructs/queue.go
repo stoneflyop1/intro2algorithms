bc7b3b05695477cb83713f5b8f6b5b41c37a2ac8
jeff <jefflyop1@gmail.com>
2016-10-08 20:59:02 +0800

    fix bug

--- datastructs/queue.go
+++ datastructs/queue.go
@@ -8,10 +8,7 @@ type Queue struct {
 
 func NewQueue(capacity int) *Queue {
 	q := new(Queue)
-	if capacity < 0 {
-		capacity = 0
-	}
-	if capacity == 0 {
+	if capacity <= 0 {
 		panic("Queue must have Capacity")
 	}
 	q.array = make([]interface{}, capacity)
@@ -20,23 +17,18 @@ func NewQueue(capacity int) *Queue {
 }
 
 func (q *Queue) Enqueue(x interface{}) {
-	if q.fixSize != 0 {
-		if q.tail == q.fixSize-1 {
-			panic("overflow")
-		}
-		q.array[q.tail] = x
-		q.tail = q.tail + 1
-	} else {
-		q.array = append(q.array, x)
-		q.tail = q.tail + 1
+	if q.tail == q.fixSize {
+		panic("overflow")
 	}
+	q.array[q.tail] = x
+	q.tail = q.tail + 1
 }
 
 func (q *Queue) Dequeue() interface{} {
-	if q.head == -1 {
+	if q.head == q.fixSize {
 		panic("underflow")
 	}
 	x := q.array[q.head]
-	q.head = q.head - 1
+	q.head = q.head + 1
 	return x
 }
--- datastructs/stack.go
+++ datastructs/stack.go
@@ -8,42 +8,35 @@ type Stack struct {
 
 func NewStack(capacity int) *Stack {
 	s := new(Stack)
-	if capacity < 0 {
-		capacity = 0
-	}
-	if capacity == 0 {
+	if capacity <= 0 {
 		panic("Stack must have Capacity")
 	}
 	s.array = make([]interface{}, capacity)
 	s.fixSize = capacity
-
+	s.top = -1
 	return s
 }
 
 func (s *Stack) IsEmpty() bool {
-	if s.top == 0 {
+	if s.top == -1 {
 		return true
 	}
 	return false
 }
 
 func (s *Stack) Push(x interface{}) {
-	if s.fixSize != 0 {
-		if s.top == s.fixSize-1 {
-			panic("overflow")
-		}
-		s.top = s.top + 1
-		s.array[s.top] = x
-	} else {
-		s.array = append(s.array, x)
-		s.top = s.top + 1
+	if s.top == s.fixSize {
+		panic("overflow")
 	}
+	s.top = s.top + 1
+	s.array[s.top] = x
 }
 
 func (s *Stack) Pop() interface{} {
 	if s.IsEmpty() {
 		panic("underflow")
 	}
+	x := s.array[s.top]
 	s.top = s.top - 1
-	return s.array[s.top+1]
+	return x
 }
