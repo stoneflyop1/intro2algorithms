5467b129c5bba022b469c36b5f0a5d2b22034e11
jeff <jefflyop1@gmail.com>
2016-11-07 21:23:54 +0800

    bubble,selection,insertion sort readme

--- sorts/BubbleSort.go
+++ sorts/BubbleSort.go
@@ -1,11 +1,21 @@
 package sorts
 
+func BubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
 // BubbleSort sort
 // 1. for i <- 1 to length[arr]
 // 2.     do for j <- length[arr] downto i+1
 // 3.         do if arr[j] < arr[j-1]
 // 4.             then exchange arr[j] <-> arr[j-1]
-func BubbleSort(arr []int) {
+func BubbleSort2(arr []int) {
 	for i := 0; i < len(arr); i++ {
 		for j := len(arr) - 1; j > i; j-- {
 			if arr[j] < arr[j-1] {
